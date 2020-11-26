6d1514492b273dca883fa4da1a9c6a8c4f852332
arrowtsvet <arrowtsvet@hotmail.com>
2020-11-26 22:49:50 +0300

    request_check_permissions

--- app/src/main/java/ru/quant/kweather/MainActivity.kt
+++ app/src/main/java/ru/quant/kweather/MainActivity.kt
@@ -1,11 +1,51 @@
 package ru.quant.kweather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private val permissionsRequestCode = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestPermissions();
+
+
+    }
+
+    private fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), permissionsRequestCode)
+        }
     }
 }
+
+/*if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+            ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionsRequestCode)
+            }
+
+
+    private fun requestPermissions (){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED)
+        {
+
+            this.requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),permissionsRequestCode)
+        }
+    }*/
+
+
